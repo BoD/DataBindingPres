fc8a36da92ab457ac77e96a62a9f3baf318d31ad
BoD <BoD@JRAF.org>
2017-08-12 14:41:56 +0200

    Add slow disk simulation

--- app/src/main/java/org/jraf/test/data/Repository.java
+++ app/src/main/java/org/jraf/test/data/Repository.java
@@ -1,13 +1,16 @@
 package org.jraf.test.data;
 
+import android.os.SystemClock;
+
 import org.jraf.test.model.User;
 
 public class Repository {
     public static User findUser(long userId) {
+        SystemClock.sleep(1000);
         return new User("John", "Smith", "jsmith@gmail.com");
     }
 
     public static void insertOrUpdateUser(User user) {
-
+        SystemClock.sleep(1000);
     }
 }
